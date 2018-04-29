c7314af67b8f5e6972c8c5512c02bcf03856a66a
Ryan Howell <ryanmatthewhowell@gmail.com>
2018-04-29 12:31:13 -0400

    Finished implementing html and css. Added js file.

---
+++ javascript.js
@@ -0,0 +1,23 @@
+//if we click on start/reset button
+  //if we are playing
+    //reload page
+  //if we are not playing
+    //set score to zero
+    //show time remaining
+    //reduce time by 1sec in loops
+      //is there time left?
+        //yes->continue
+        //no->game over
+    //change button text to reset
+    //generate new Q&A
+
+
+//if we click on an answer box
+  //if we are playing
+    //correct?
+      //yes
+        //increase the score
+        //show correct box for 1sec
+        //generate new Q&A
+      //no
+        //show try again box for 1sec
